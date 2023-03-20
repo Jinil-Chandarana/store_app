ac0d994e09606e00937270d7a961751e99780728
Jinil-Chandarana <jinilchandarana@gmail.com>
2023-03-20 17:37:37 +0530

    filters globally

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/providers/products.dart';
+import 'package:provider/provider.dart';
 import '../widgets/products_grid.dart';
+
+enum FilterOptions { Favourite, All }
 
 class ProductsOverviewScreen extends StatelessWidget {
   // ProductsOverviewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final productContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: const Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              if (selectedValue == FilterOptions.Favourite) {
+                productContainer.showFavouritesOnly();
+              } else {
+                productContainer.showAll();
+              }
+            },
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                  child: Text('Only Favourites'),
+                  value: FilterOptions.Favourite),
+              PopupMenuItem(child: Text('Show all'), value: FilterOptions.All)
+            ],
+            icon: Icon(Icons.more_vert),
+          )
+        ],
       ),
       body: ProductGrid(),
     );
